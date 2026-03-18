448333f3a016b78aded31e7d2c2dff2c6fc6d182
Nalugsw <00001128535750sp@al.educacao.sp.gov.br>
2026-03-18 13:23:19 -0300

    alteração de cor

--- CalculoSalario/Form1.Designer.cs
+++ CalculoSalario/Form1.Designer.cs
@@ -40,13 +40,13 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.button3 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -63,7 +63,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("PMingLiU-ExtB", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(278, 109);
+            this.label2.Location = new System.Drawing.Point(265, 108);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(262, 35);
             this.label2.TabIndex = 1;
@@ -73,7 +73,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("PMingLiU-ExtB", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(278, 203);
+            this.label3.Location = new System.Drawing.Point(265, 202);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(214, 35);
             this.label3.TabIndex = 2;
@@ -83,7 +83,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("PMingLiU-ExtB", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(278, 295);
+            this.label4.Location = new System.Drawing.Point(265, 294);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(328, 35);
             this.label4.TabIndex = 3;
@@ -93,7 +93,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("PMingLiU-ExtB", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(245, 444);
+            this.label5.Location = new System.Drawing.Point(265, 440);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(200, 35);
             this.label5.TabIndex = 4;
@@ -101,7 +101,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(607, 106);
+            this.textBox1.Location = new System.Drawing.Point(594, 105);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(116, 38);
@@ -110,7 +110,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(607, 203);
+            this.textBox2.Location = new System.Drawing.Point(594, 202);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(116, 38);
@@ -119,7 +119,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(607, 292);
+            this.textBox3.Location = new System.Drawing.Point(594, 291);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(116, 38);
@@ -128,27 +128,52 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(426, 352);
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Cyan;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("PMingLiU-ExtB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(410, 354);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 52);
             this.button1.TabIndex = 8;
             this.button1.Text = "CALCULAR";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightCoral;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ImageKey = "84_Sem_Titulo_20260317181800.png";
             this.button2.ImageList = this.imageList1;
             this.button2.Location = new System.Drawing.Point(888, 615);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(67, 67);
             this.button2.TabIndex = 9;
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "84_Sem_Titulo_20260317181800.png");
+            this.imageList1.Images.SetKeyName(1, "84_Sem_Titulo_20260317181603.png");
+            this.imageList1.Images.SetKeyName(2, "Design sem nome.png");
+            this.imageList1.Images.SetKeyName(3, "Design-sem-nome.ico");
+            this.imageList1.Images.SetKeyName(4, "icon (1).png");
+            this.imageList1.Images.SetKeyName(5, "icon (2).png");
+            this.imageList1.Images.SetKeyName(6, "icon (3).png");
+            this.imageList1.Images.SetKeyName(7, "icon.png");
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button3.BackColor = System.Drawing.Color.White;
             this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGoldenrod;
@@ -165,7 +190,7 @@
             // label6
             // 
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label6.Location = new System.Drawing.Point(493, 444);
+            this.label6.Location = new System.Drawing.Point(513, 440);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(197, 45);
             this.label6.TabIndex = 11;
@@ -174,7 +199,7 @@
             // label7
             // 
             this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label7.Location = new System.Drawing.Point(493, 541);
+            this.label7.Location = new System.Drawing.Point(513, 537);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(197, 45);
             this.label7.TabIndex = 13;
@@ -184,7 +209,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("PMingLiU-ExtB", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(245, 551);
+            this.label8.Location = new System.Drawing.Point(265, 547);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(229, 35);
             this.label8.TabIndex = 12;
@@ -193,7 +218,7 @@
             // label9
             // 
             this.label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label9.Location = new System.Drawing.Point(493, 637);
+            this.label9.Location = new System.Drawing.Point(513, 633);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(197, 45);
             this.label9.TabIndex = 15;
@@ -203,23 +228,17 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("PMingLiU-ExtB", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(245, 647);
+            this.label10.Location = new System.Drawing.Point(265, 643);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(161, 35);
             this.label10.TabIndex = 14;
             this.label10.Text = "Descontos:";
             // 
-            // imageList1
-            // 
-            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList1.Images.SetKeyName(0, "84_Sem_Titulo_20260317181800.png");
-            this.imageList1.Images.SetKeyName(1, "84_Sem_Titulo_20260317181603.png");
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
             this.ClientSize = new System.Drawing.Size(1008, 729);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label10);
